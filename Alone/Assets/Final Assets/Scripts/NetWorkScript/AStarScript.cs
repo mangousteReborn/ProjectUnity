35034ea83e97abac57e812c659657d33a461519a
Lecomte <emerick.lecomte@gmail.com>
2014-11-14 16:44:48 +0100

    Networking Deplacement Correctif

--- Alone/Assets/Final Assets/Scripts/NetWorkScript/AStarScript.cs
+++ Alone/Assets/Final Assets/Scripts/NetWorkScript/AStarScript.cs
@@ -12,23 +12,16 @@ public class AStarScript {
         this.networkView = view;
     }
 
-    public NavMeshPath setTarget(Vector3 startPos,Vector3 newTarget)
+    public NavMeshPath setTarget(Vector3 startPos,Vector3 newTarget,bool isFormRPC)
     {
-        if (Network.isServer)
-        {
-            pathToTargetCorner = getCalcPath(startPos, newTarget);
+        pathToTargetCorner = getCalcPath(startPos, newTarget);
 
-            //Networking
-            string pathCorner = "";
-            if (pathToTargetCorner != null)
-            {
-                foreach (Vector3 corner in pathToTargetCorner.corners)
-                    pathCorner += corner.x.ToString() + "," + corner.y.ToString() + "," + corner.z.ToString() + ";";
-                networkView.RPC("changeDirection", RPCMode.Others, networkView.viewID, pathCorner);
-            }
-            return pathToTargetCorner;
+        //Networking
+        if (pathToTargetCorner != null && (!isFormRPC || Network.isServer))
+        {
+            networkView.RPC("setTargetRPC", RPCMode.Others, networkView.viewID, startPos, newTarget);
         }
-        return null;
+        return pathToTargetCorner;
     }
 
     private NavMeshPath getCalcPath(Vector3 origin, Vector3 wantToGo)
@@ -55,18 +48,4 @@ public class AStarScript {
             deplacementScript.changeDirection(pathCorner);
         }  
      */
-
-
-    public NavMeshPath changeDirection(string pathCorner)
-    {
-        string[] arrayCorner = pathCorner.Split(';');
-        pathToTargetCorner = new NavMeshPath();
-        for (int i = 0; i < arrayCorner.Length-1;i++ )
-        {
-            string[] cornerFloat = arrayCorner[i].Split(',');
-            Vector3 cornerVector = new Vector3(float.Parse(cornerFloat[0]), float.Parse(cornerFloat[1]), float.Parse(cornerFloat[2]));
-            pathToTargetCorner.corners.SetValue(cornerVector,i);
-        }
-        return pathToTargetCorner;
-    }
 }

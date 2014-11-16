5478e077cc45bc0358d7db20b835c8bd527b343a
Tecnop^ <mr_p_75@hotmail.fr>
2014-11-16 21:27:29 +0100

    Popup damages + Effect++

---
+++ Alone/Assets/Final Assets/Scripts/Components/DamagePopupScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DamagePopupScript : MonoBehaviour {
+
+	[SerializeField]
+	Animator _animator;
+
+	[SerializeField]
+	GameObject _popup;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	public void destroy(){
+		Destroy (_popup);
+	}
+}
--- Alone/Assets/Final Assets/Scripts/PlayerScript/CharacterManager.cs
+++ Alone/Assets/Final Assets/Scripts/PlayerScript/CharacterManager.cs
@@ -13,9 +13,15 @@ public class CharacterManager : MonoBehaviour {
 	
 	CharacterStats _characterStats;
 		
+	[SerializeField]
+	private GameObject _damagePopup;
 
 	[SerializeField]
-	public GameObject _healthBar;
+	private GameObject _healthBar;
+
+	private Scrollbar _healthBarScrollbar;
+	private Text _healthBarLabel;
+		
 
 	[SerializeField]
 	public GameObject _character;
@@ -23,18 +29,60 @@ public class CharacterManager : MonoBehaviour {
 	void Start () {
 		this._characterStats = new CharacterStats ();
 
-		this._healthBar = (GameObject)Instantiate (this._healthBar);
-		RectTransform r = this._healthBar.GetComponent<RectTransform>();
-		r.position = this._character.transform.position;
+		// HealthBar init (if defined)
+		if (this._healthBar != null) {
 
+			this._healthBar = (GameObject)Instantiate (this._healthBar);
+			RectTransform r = this._healthBar.GetComponent<RectTransform>();
+			r.position = this._character.transform.position;
+
+			this._healthBarScrollbar = this._healthBar.GetComponentInChildren<Scrollbar>();
+			this._healthBarLabel = this._healthBar.GetComponentInChildren<Text>();
+
+			this._characterStats.listenersList.Add(updateHealthBar);
+
+			this._healthBar.transform.parent = this._character.transform;
+		}
+
+		this._characterStats.register (CharacterStatsEvent.currentLifeChange, createPopup);
+
+
+		this._characterStats.pushEffect(new EffectDamageOverBattle(10, 3));
+
+		this._characterStats.hasChanged();
+	}
+	
+	private void updateHealthBar(CharacterStats stats,object[] param){
+		this._healthBarScrollbar.size = (float)stats.currentLife / (float)stats.maxLife;
+		this._healthBarLabel.text = stats.currentLife + " / " + stats.maxLife;
 
 	}
 
+	private void createPopup(CharacterStats stats, object[] param){
+		int damages = (int)param [0] - (int)param [1];
+		if (damages >= 0) {
+			GameObject popup = (GameObject)Instantiate (this._damagePopup);
+			popup.GetComponentInChildren<Text> ().text = ""+damages;
+		}
+	}
+
+	public void dealDamages(int damages){
+		this._characterStats.currentLife -= damages;
+
+	}
+
+	// Get / Seters
+	public CharacterStats characterStats{
+		get {
+			return this._characterStats;
+		}
+	}
 
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown (KeyCode.T)) {
-			this._characterStats.pushEffect(new EffectBonusLife(100));
+
+			this._characterStats.updateCombatEffects();
 		}
 	}
 }

6e2d46c780e2e1ed6198a0cd34fd5fb1067fc51b
tecnop <mr_p_75@hotmail.fr>
2014-12-03 13:10:26 +0100

    Minor changes

--- Alone/Assets/Final Assets/Scripts/Components/GUI/PlayerGUIScript.cs
+++ Alone/Assets/Final Assets/Scripts/Components/GUI/PlayerGUIScript.cs
@@ -1,8 +1,15 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
-
-public class GUIScript : MonoBehaviour {
+/*
+ *  author: Thomas P
+ * 	created_at:27/11/14
+ * 
+ * /?\ : Classic Player Interface (whole)
+ * General and complete interface for Classic players (Game Master will get
+ * another GUI).
+ */
+public class PlayerGUIScript : MonoBehaviour {
 
 	[SerializeField]
 	GameObject _mainContainerObject;
@@ -24,8 +31,7 @@ public class GUIScript : MonoBehaviour {
 
 	private int _screenHeight;
 	private int _screenWidth;
-
-
+	
 
 	// MainCanvas
 	private Canvas _mainCanvas;
@@ -43,17 +49,18 @@ public class GUIScript : MonoBehaviour {
 			//MainMenu
 			private Canvas _mainMenu;
 
+		
 
 	void Start() {
 		_screenHeight = Screen.height;
 		_screenWidth = Screen.width;
-
-		_mainCanvas = (Canvas) Instantiate(_mainObject);
+		
+		_mainCanvas = _mainContainerObject.GetComponent<Canvas>();
 		RectTransform rt = _mainCanvas.GetComponent<RectTransform>();
-		rt.rect.height = _screenHeight;
-		rt.rect.width = _screenWidth;
-
-
-
+		
+		// Must store Rect typed objert in a variable. Dunno why .. !
+		Rect r = rt.rect;
+		r.height = _screenHeight;
+		r.width = _screenWidth;
 	}
 }
--- Alone/Assets/Final Assets/Scripts/GameManager.cs
+++ Alone/Assets/Final Assets/Scripts/GameManager.cs
@@ -3,11 +3,20 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour {
 	// STUFF STUFF !!
+	
+	[SerializeField]
+	GameObject _playerGUIObject;
+	// TODO : GameMasterGUIScript !!
 
+	private PlayerGUIScript _playerGUIScript;
 
 
 
 	void Start () {
 		GameData.init();
+		// TODO : Check if current player is "classic" or "game master"
+		//_playerGUIScript = (PlayerGUIScript) Instantiate(_playerGUIObject);
+		_playerGUIObject = (GameObject)Instantiate(_playerGUIObject);
+		_playerGUIScript = _playerGUIObject.GetComponent<PlayerGUIScript>();
 	}
 }

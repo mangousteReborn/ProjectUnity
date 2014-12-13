917d6c5c783ef1e8bb1aa79d3de5746012a53dee
Tecnop^ <mr_p_75@hotmail.fr>
2014-12-13 23:26:10 +0100

    Fix Fix

---
+++ Alone/Assets/Final Assets/Scripts/Components/GUI/GameMasterGUIScript.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+/*
+ *  author: Thomas P
+ * 	created_at:13/12/14
+ * 
+ * /?\ : Game Master Interface (whole)
+ * 
+ * 
+ * 
+ */
+
+public class GameMasterGUIScript : MonoBehaviour, IPlayerGUI{
+	[SerializeField]
+	GameObject _mainContainerObject;
+
+		[SerializeField]
+		GameObject _topContainerObject;
+		
+			[SerializeField]
+			GameObject _gameStateObject;
+
+			[SerializeField]
+			GameObject _readyButtonObject;
+
+	
+	// Independents comps
+	[SerializeField]
+	GameObject _VignettesPickerObject;
+
+	private int _screenHeight;
+	private int _screenWidth;
+
+	private GameObject _unitsVignettesPicker;
+
+	// Use this for initialization
+	void Start () {
+		this._screenHeight = Screen.height;
+		this._screenWidth = Screen.width;
+
+
+		// Bonus Vignettes picker def
+		this._unitsVignettesPicker = (GameObject)Instantiate (this._VignettesPickerObject);
+		this._unitsVignettesPicker.transform.parent = this._mainContainerObject.transform;
+		this._unitsVignettesPicker.GetComponent<RectTransform> ().anchoredPosition = new Vector2 (0, 0);
+		this._unitsVignettesPicker.GetComponent<VignettesPickerScript> ().setDimension (this._screenWidth, 50);
+		this._unitsVignettesPicker.GetComponent<VignettesPickerScript> ().setPlayerGUI (this);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void changeGameMode(uint gameMode){
+		return;
+	}
+
+	public void setCharacterManager (CharacterManager cm){
+		return;
+	}
+}

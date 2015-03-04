c1990585e26cee27f80c3d432c26c667afdb00ad
Lecomte <emerick.lecomte@gmail.com>
2015-03-04 21:09:35 +0100

    menu

---
+++ Alone/Assets/Final Assets/Scripts/MenuScript/setIpScript.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class setIpScript : MonoBehaviour {
+
+    public StaticVariableScript script;
+
+    public InputField nameInputField = null;
+
+    public Button submitButton = null;
+
+    public LobbyScript lobby;
+    public Canvas lobbyCanvas;
+    public Canvas myCanvas;
+
+    private void Start()
+    {
+        //nameInputField.onSubmit.AddListener((value) => SubmitName(value));
+        submitButton.onClick.AddListener(() => SubmitName(nameInputField.value));
+    }
+
+    private void SubmitName(string ip)
+    {
+        script.setIp(ip);
+        if (StaticVariableScript.isServer)
+        {
+            if (script.createServer())
+            {
+                myCanvas.gameObject.SetActive(false);
+                lobbyCanvas.gameObject.SetActive(true);
+                lobby.addPlayer();
+            }
+        }
+        else
+        {
+            script.ConnectToServer();
+        }
+    }
+
+    void OnConnectedToServer()
+    {
+        myCanvas.gameObject.SetActive(false);
+        lobbyCanvas.gameObject.SetActive(true);
+        networkView.RPC("newPlayerConnected", RPCMode.All, null);
+    }
+}
